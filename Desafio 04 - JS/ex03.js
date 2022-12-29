9d1d8b04bc0d1e1000e6f6338460e4e95232aec7
gabrielandre20 <116749433+gabrielandre20@users.noreply.github.com>
2022-12-29 12:26:53 -0300

    Praticando JS

--- Desafio 04 - JS/ex03.js
+++ Desafio 04 - JS/ex03.js
@@ -12,13 +12,36 @@ function transformDegree (degree) {
     const celsiusExists = degree.toUpperCase().includes('C')
     const fahrenheitExists = degree.toUpperCase().includes('F')
 
-    if(!celsiusExits && !fahrenheitExists) {
+    if(!celsiusExists && !fahrenheitExists) {
             throw new Error ('Grau não identificado')
+    }
+
+    // fluxo ideal F to C
+    let updatedDegree = Number(degree.toUpperCase().replace('F', ''))
+    let formula = (fahrenheit) => (fahrenheit - 32) * 5/9
+    let degreeSign = 'C'
+
+    //fluxo C to F
+    if(celsiusExists) {
+    updatedDegree = Number(degree.toUpperCase().replace('C', ''))
+    formula = celsius => celsius *  9/5 + 32
+    degreeSign = 'F'
+    }
+
+
+
+
+
+    return formula(updatedDegree) + degreeSign
 }
 
-try { 
-    transformDegree('50F')
-    
-} catch {
 
+
+try { 
+    console.log(transformDegree('10C'))
+    console.log(transformDegree('50F'))
+    //transformDegree('50Z')
+    
+} catch (error) {
+    console.log(error.message)
 }
