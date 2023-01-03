ddc8b18800462f491ecb2509a7f00d69eb927c4d
gabrielandre20 <116749433+gabrielandre20@users.noreply.github.com>
2023-01-03 09:42:28 -0300

    Update ex04.js

--- Desafio 04 - JS/ex04.js
+++ Desafio 04 - JS/ex04.js
@@ -50,5 +50,43 @@ const booksByCategory = [
     }
 ];
 
-console.log(booksByCategory.length)
-console.log(booksByCategory.filter({category}))
+const totalCategories = booksByCategory.length
+console.log(totalCategories)
+for ( let category of booksByCategory) {
+    console.log('Total de livros da categoria ', category.category)
+    console.log(category.books.length)
+}
+
+function countAuthors() {
+    let authors = [];
+
+    for ( let category of booksByCategory) {
+        for (let book of category.books) {
+            if(authors.indexOf(book.author) == -1) {
+                authors.push(book.author)
+            }
+        }
+    }
+
+    console.log('Total de autores: ', authors.length)
+
+}
+
+countAuthors();
+
+function booksOfAuthor(author) {
+    let books = [];
+
+    for ( let category of booksByCategory) {
+        for (let book of category.books) {
+            if(book.author === author) {
+                books.push(book.title)
+            }
+        }
+    }
+
+    console.log(`Livros do autor ${author}: ${books.join(", ")}`)
+
+}
+
+booksOfAuthor('George S. Clason')
